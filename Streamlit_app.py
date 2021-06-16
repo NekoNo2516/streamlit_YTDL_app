8e4b575d8c93b076e4e8b3b374ba40bf496ddc18
NekoNo2516 <85822949+NekoNo2516@users.noreply.github.com>
2021-06-16 23:49:46 +0800

    Add files via upload

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,4 +1,4 @@
-#导入streamlit制作网页#Use Streanlit to build a web app!
+    #导入streamlit制作网页#Use Streanlit to build a web app!
 import streamlit as st
 #导入pytube来爬取油管视频信息#Use pytube to crawl YouTube
 import pytube
@@ -16,11 +16,14 @@ import easygui as g
 import ffmpeg
 import sys
 import os
-
+#异常判断相关
+from urllib import request
+from urllib import error
+from urllib.request import urlopen
 
 
 #通过不同复选框筛选下载选项
-def options_filter(a,b,c,d):
+def options_filter(a,b,c,d,language):
     row1, row2, row3, row4, row5 = st.beta_columns((1,1,1,1,1))
 
     with row1:
@@ -67,8 +70,14 @@ def options_filter(a,b,c,d):
 
             #当有可用字幕时在路径保存字幕srt文件#Save Sub if it's available
             if c_tag<10:
-                content = caption.generate_srt_captions()
-                makefile(my_path,content,language)
+                try:
+                    content = caption.generate_srt_captions()
+                    makefile(my_path,content,language)
+                except UnboundLocalError:
+                    content = "I'm sorry~~There's no YouTube subtitle available."
+                    language = "Not found"
+                    makefile(my_path,content,language)
+                    
             #无字幕可用时#When no subtitle is available...
             else:
                 content = "I'm sorry~~There's no YouTube subtitle available."
@@ -76,14 +85,14 @@ def options_filter(a,b,c,d):
                 makefile(my_path,content,language)
             
 
-#过滤字幕选项#filtering the subtitles
+#过滤字幕选项#Filtering the subtitles
 def get_caption_by_language_name(yt, lang_name):
 	for caption in yt.caption_tracks:
 		if caption.name == lang_name:
 			return caption
 		    
 
-#创建srt格式字幕文件并输入文本内容#create a SRT file to save the subtitle
+#创建srt格式字幕文件并输入文本内容#Create a SRT file to save the subtitle
 def makefile(path,content,language):
     if os.path.exists(path):
         if os.path.isdir(path):
@@ -122,24 +131,16 @@ def StrofSize(size):
 st.set_page_config(page_title=None, page_icon=None, layout='wide', initial_sidebar_state='auto')
 
 #默认打开侧边框#Open the sidebar by default
-row12, row22, row32 = st.sidebar.beta_columns((9,4,18))
-
-with row12:
-    st.text('')
-    st.button("Subtitle\nLanguage")
-    
-with row22:
-    st.text('')
-    
-with row32:
-    language = st.select_slider(
-         "",
-         options=['English', 'Japanese', 'Chinese'])
+#提供选项框来让用户选择字幕语言#Selectbox for Sub language choosing
+language = st.sidebar.selectbox(
+            "📥 Choose the language of sub you need.",
+            ('English', 'Chinese', 'Japanese'),
+            help = " Choose Sub language here.")
 
 #设置网站的墙纸？只是为了美观而已加了一张图片
 try:
     image1=Image.open("C:/Users/David Wu/Pictures/Screenshots/屏幕截图(1).png")#You can choose your own wallpaper here, or not.
-    st.image(image1, use_column_width=True)
+    st.image(image1, use_column_width = True)
 except FileNotFoundError:
     #If you open the web app without setting a wallpaper...
     st.write("💭 Don't you think the web UI is too plain & we can add a beanutifual wallpaper here?")
@@ -164,130 +165,143 @@ with row1_2:
 if url != '':
     try:
         my_video=YouTube(url)
+        url_test = request.Request(url)
+        urlopen(url_test)
     except pytube.exceptions.RegexMatchError:
         #当用户输入了错误的url时提示报错#print error log to the user if the URL isn't usable
         st.error("Please enter YouTube URL!")
         url_capability = False
+    except error.URLError:
+        st.error("「たぶん」VPN Problem ⁉️")
+        url_capability = False
     else:
         url_capability = True
 
-#当用户输入了正确的油管网址#If the URL work fine
-if url!='' and url_capability:    
-    #先对用户输入进行反馈#Feedback to the user before python crawling...
-    st.write("")
-    st.write("🌟GOT The Video INFO‼ Check it out!")
-    st.write("")
-    
-    #显示视频标题#Video Title
-    st.write('''🎞︎''',my_video.title)
-
-    row2_1, row2_2 = st.beta_columns((14,15))
-    
-    with row2_1:
-        #封面预览:将视频封面图用skimage通过URL读取并展示在网页界面上#Thumbnail Previewing?
-        image2 = io.imread(my_video.thumbnail_url)
-        st.image(image2, width=420)
+try:
+    #当用户输入了正确的油管网址#If the URL work fine
+    if url!='' and url_capability:
+        #Find out the HTTPError first
+        testVAR = my_video.title
         
-    with row2_2:
-        #显示视频的作者和时长#The author and time length of the video
-        st.header("")
-        st.write("Author—",my_video.author)
-        st.write("Length*(of time)*—",
-            dt.timedelta(seconds=my_video.length))
+        #先对用户输入进行反馈#Feedback to the user before python crawling...
+        st.write("")
+        st.success("🌟GOT The Video INFO‼ Check it out!")
+        st.write("")
+        
+        #显示视频标题#Video Title
+        st.write('''🎞︎''',my_video.title)
 
-        #提供复选框让用户筛选下载选项#默认情况下给出的下载选项是720p的mp4视频
-        options = st.multiselect(
-            "",
-            ['Only Audio', 'Only Video','A/V','2160p','1440p','1080p', '720p','360p'],
-            ['720p', 'Only Audio'])
+        row2_1, row2_2 = st.beta_columns((14,15))
+        
+        with row2_1:
+            #封面预览:将视频封面图用skimage通过URL读取并展示在网页界面上#Thumbnail Previewing?
+            image2 = io.imread(my_video.thumbnail_url)
+            st.image(image2, width=420)
+            
+        with row2_2:
+            #显示视频的作者和时长#The author and time length of the video
+            st.header("")
+            st.write("Author—",my_video.author)
+            st.write("Length*(of time)*—",
+                dt.timedelta(seconds=my_video.length))
 
-    #在侧边显示视频发布日期和播放量
-    st.sidebar.title("**【_More Informations_】**")
-    st.sidebar.write("🔸PublishDate—",my_video.publish_date)
-    st.sidebar.write("🔸Views—",my_video.views,"times")
-     
-    #显示视频封面的URL以便下载
-    st.sidebar.markdown("🔸Thumbnail_URL: ")
-    st.sidebar.markdown(my_video.thumbnail_url)
+            #提供复选框让用户筛选下载选项#默认情况下给出的下载选项是720p的mp4视频
+            options = st.multiselect(
+                "",
+                ['Only Audio', 'Only Video','A/V','2160p','1440p','1080p', '720p','360p'],
+                ['720p', 'Only Audio'])
 
-    #用复选框显示视频的关键词#默认显示前四个关键词
-    if len(my_video.keywords)>=4:
-        st.sidebar.multiselect(
-            "🔸Keywords:",
-            my_video.keywords,
-            [my_video.keywords[0],my_video.keywords[1],my_video.keywords[2],my_video.keywords[3]],
-            key=1111)
-    #如果关键词很少则全部输出#如果没有关键词提示无关键词
-    elif len(my_video.keywords)>0:
-        st.sidebar.multiselect(
-            "🔸Keywords:",
-            my_video.keywords,
-            my_video.keywords,
-            key=1112)
+        #在侧边显示视频发布日期和播放量#Show the Publishdate&Views in the sidebar
+        st.sidebar.title("**【_More Informations_】**")
+        st.sidebar.write("🔸PublishDate—",my_video.publish_date)
+        st.sidebar.write("🔸Views—",my_video.views,"times")
+         
+        #显示视频封面的URL以便下载#Also the thumbnail_URL
+        st.sidebar.markdown("🔸Thumbnail_URL: ")
+        st.sidebar.markdown(my_video.thumbnail_url)
+
+        #用复选框显示视频的关键词#默认显示前四个关键词
+        if len(my_video.keywords)>=4:
+            st.sidebar.multiselect(
+                "🔸Keywords:",
+                my_video.keywords,
+                [my_video.keywords[0],my_video.keywords[1],my_video.keywords[2],my_video.keywords[3]],
+                key=1111)
+        #如果关键词很少则全部输出#如果没有关键词提示无关键词
+        elif len(my_video.keywords)>0:
+            st.sidebar.multiselect(
+                "🔸Keywords:",
+                my_video.keywords,
+                my_video.keywords,
+                key=1112)
+        else:
+            st.sidebar.write("🔸Keywords:") 
+            st.text("SORRY~~This video has NO keywords.")
+            
+
+        #用代码框的形式输出视频简介以减少简介面积避免喧宾夺主
+        st.sidebar.write("🔸Description:")
+        st.sidebar.code(my_video.description,language=None)
+
+        ###########################Download Interface############################
+        
+        row1, row2, row3, row4, row5 = st.beta_columns((1,1,1,1,1))#显示各列信息名
+
+        with row1:
+            st.code("Format")
+        with row2:
+            st.code("Resolution")
+        with row3:
+            st.code("Audio Info")
+        with row4:
+            st.code("File Size")
+        with row5:
+            st.code("DownloadLink")
+
+        #针对复选框不同的标签过滤下载选项
+        ###'Onlu Audio'标签会给出一个纯音频的文件#默认为160kbps#备用70kbps
+        if 'Only Audio' in options and my_video.streams.get_by_itag(251)!=None:
+            options_filter("WEBM","No","160kbps",251,language)
+        if 'Only Audio' in options and my_video.streams.get_by_itag(251)==None:
+            options_filter("WEBM","No","70kbps",250,language)
+
+        ###'2160p'标签仅有纯视频选项#油管将高分辨率视频全部拆分为音视频两个文件#提供MP4和WEBM两种格式
+        if '2160p' in options and 'A/V' not in options and my_video.streams.get_by_itag(313)!=None :
+            options_filter("WEBM",'2160p','No',313,language)
+        if '2160p' in options and 'A/V' not in options and my_video.streams.get_by_itag(401)!=None :
+            options_filter("MP4",'2160p','No',401,language)
+
+        ###'1440p'选项同'2160p'
+        if '1440p' in options and 'A/V' not in options and my_video.streams.get_by_itag(271)!=None :
+            options_filter("WEBM",'1440p','No',271,language)
+        if '1440p' in options and 'A/V' not in options and my_video.streams.get_by_itag(400)!=None :
+            options_filter("MP4",'1440p','No',400,language)
+
+        ##'1080p'选项同'2160p'
+        if '1080p' in options and 'A/V' not in options and my_video.streams.get_by_itag(248)!=None :
+            options_filter("WEBM",'1080p','No',248)
+        if '1080p' in options and 'A/V' not in options and my_video.streams.get_by_itag(137)!=None :
+            options_filter("MP4",'1080p','No',137)
+
+        ##'720p'及以下的油管视频有音视频为一个文件#此处提供纯视频和音视频选项
+        if '720p' in options and 'A/V' not in options and my_video.streams.get_by_itag(247)!=None :
+            options_filter("WEBM",'720p','No',247,language)
+        if '720p' in options and 'A/V' not in options and my_video.streams.get_by_itag(136)!=None :
+            options_filter("MP4",'720p','No',136,language)
+        if '720p' in options and 'Only Video' not in options and my_video.streams.get_by_itag(22)!=None :
+            options_filter("MP4",'720p','Yes',22,language)
+
+        ##'360p'仅给出音视频选项
+        if '360p' in options and 'Only Video' not in options and my_video.streams.get_by_itag(18)!=None :
+            options_filter("MP4","360p","Yes",18,language)
+
     else:
-        st.sidebar.write("🔸Keywords:") 
-        st.text("SORRY~~This video has NO keywords.")
+        #使用python代码文本格式输出爬虫限制信息
+        code1='''#Caution: Downloading some of music videos and copyrighted content is restricted.'''
+        st.text(code1)
         
+#if the internet is taking shirking...
+except error.HTTPError:
+    st.error("⚠︎ The Internet may be shirking~ Please click F5 to rerun the Web.")
 
-    #用代码框的形式输出视频简介以减少简介面积避免喧宾夺主
-    st.sidebar.write("🔸Description:")
-    st.sidebar.code(my_video.description,language=None)
-
-    ###########################下载界面##########################
-    
-    row1, row2, row3, row4, row5 = st.beta_columns((1,1,1,1,1))#显示各列信息名
-
-    with row1:
-        st.code("Format")
-    with row2:
-        st.code("Resolution")
-    with row3:
-        st.code("Audio Info")
-    with row4:
-        st.code("File Size")
-    with row5:
-        st.code("DownloadLink")
-
-    #针对复选框不同的标签过滤下载选项
-    ###'Onlu Audio'标签会给出一个纯音频的文件#默认为160kbps#备用70kbps
-    if 'Only Audio' in options and my_video.streams.get_by_itag(251)!=None:
-        options_filter("WEBM","No","160kbps",251)
-    if 'Only Audio' in options and my_video.streams.get_by_itag(251)==None:
-        options_filter("WEBM","No","70kbps",250)
-
-    ###'2160p'标签仅有纯视频选项#油管将高分辨率视频全部拆分为音视频两个文件#提供MP4和WEBM两种格式
-    if '2160p' in options and 'A/V' not in options and my_video.streams.get_by_itag(313)!=None :
-        options_filter("WEBM",'2160p','No',313)
-    if '2160p' in options and 'A/V' not in options and my_video.streams.get_by_itag(401)!=None :
-        options_filter("MP4",'2160p','No',401)
-
-    ###'1440p'选项同'2160p'
-    if '1440p' in options and 'A/V' not in options and my_video.streams.get_by_itag(271)!=None :
-        options_filter("WEBM",'1440p','No',271)
-    if '1440p' in options and 'A/V' not in options and my_video.streams.get_by_itag(400)!=None :
-        options_filter("MP4",'1440p','No',400)
-
-    ##'1080p'选项同'2160p'
-    if '1080p' in options and 'A/V' not in options and my_video.streams.get_by_itag(248)!=None :
-        options_filter("WEBM",'1080p','No',248)
-    if '1080p' in options and 'A/V' not in options and my_video.streams.get_by_itag(137)!=None :
-        options_filter("MP4",'1080p','No',137)
-
-    ##'720p'及以下的油管视频有音视频为一个文件#此处提供纯视频和音视频选项
-    if '720p' in options and 'A/V' not in options and my_video.streams.get_by_itag(247)!=None :
-        options_filter("WEBM",'720p','No',247)
-    if '720p' in options and 'A/V' not in options and my_video.streams.get_by_itag(136)!=None :
-        options_filter("MP4",'720p','No',136)
-    if '720p' in options and 'Only Video' not in options and my_video.streams.get_by_itag(22)!=None :
-        options_filter("MP4",'720p','Yes',22)
-
-    ##'360p'仅给出音视频选项
-    if '360p' in options and 'Only Video' not in options and my_video.streams.get_by_itag(18)!=None :
-        options_filter("MP4","360p","Yes",18)
-  
-
-else:
-    #使用python代码文本格式输出爬虫限制信息
-    code1='''#Caution: Downloading some of music videos and copyrighted content is restricted.'''
-    st.text(code1)
 
